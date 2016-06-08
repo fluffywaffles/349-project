b735088e673181af01f3001ec71c81a841886733
MichaelJH <MichaelHorst2016@u.northwestern.edu>
2016-06-08 00:25:39 -0500

    Added scripts for generating text files of top x game with and without name prepended

---
+++ scripts/top50+name.py
@@ -0,0 +1,48 @@
+# from bs4 import BeautifulSoup, Comment
+import json #to use json files
+import codecs # to open a text file in utf-8
+import urllib
+import pandas # To extract data from the csv and have robust functionality
+import glob # To iterate through files in a directory
+
+
+colnames = ['url','reviewerUsername','reviewerDisplayName','reviewerUrl','reviewerProductsCount',\
+'reviewText','date','recommended','hoursPlayed','voteText','helpfulPercent','votesHelpful'\
+,'votesTotal','votesFunny','commentsCount']
+
+# get game ids and names
+gamecols = ['appid','name','release','reviewCount','reviewScore','reviewSentiment',\
+'reviewSummary','url']
+gameInfo = pandas.read_csv("../data/gamedata-no-dupes.csv")
+gameIDs = gameInfo.appid.tolist()
+gameNames = gameInfo.name.tolist()
+
+text_file = codecs.open("../data/top_3+name.txt", "w", "ascii") #creates a text file for the particular game
+for game in glob.iglob('../data/reviews/*.csv'):
+	
+	data = pandas.read_csv(game, names=colnames)
+	reviewsText = data.reviewText.tolist()
+
+	# get the name of the game given the app id
+	gameID = int(game[16:-4]) # file name minus the .csv - 16 for ../data/reviews/
+	if gameID in gameIDs:
+		index = gameIDs.index(gameID)
+		name = gameNames[index].decode('ascii', errors='ignore')
+	else:
+		name = "NoName"
+
+	if len(reviewsText) > 100:
+		#splitting about the '\n'
+		nl = '\n'
+		for review in reviewsText[1:4]:
+			# write the game name before each review
+			text_file.write(name)
+			text_file.write('\n')
+			# decode the review
+			review = str(review).decode('ascii', errors = 'ignore')
+			review = nl.join(review.split('\\n'))
+			# write review to a text file
+			text_file.write(review)
+			text_file.write('\n\n')
+
+text_file.close();
---
+++ scripts/top50.py
@@ -0,0 +1,30 @@
+# from bs4 import BeautifulSoup, Comment
+import json #to use json files
+import codecs # to open a text file in utf-8
+import urllib
+import pandas # To extract data from the csv and have robust functionality
+import glob
+
+# extracting a list of game ids from the csv
+colnames = ['url','reviewerUsername','reviewerDisplayName','reviewerUrl','reviewerProductsCount',\
+'reviewText','date','recommended','hoursPlayed','voteText','helpfulPercent','votesHelpful'\
+,'votesTotal','votesFunny','commentsCount']
+
+text_file = codecs.open("../data/top_3.txt", "w", "ascii") #creates a text file for the particular game
+for game in glob.iglob('../data/reviews/*.csv'):
+
+	data = pandas.read_csv(game, names=colnames)
+	reviewsText = data.reviewText.tolist()
+
+	if len(reviewsText) > 100:
+		#splitting about the '\n'
+		nl = '\n'
+		for review in reviewsText[1:4]:
+
+			review = str(review).decode('ascii', errors = 'ignore')
+			review = nl.join(review.split('\\n'))
+			#writing to a text file
+			text_file.write(review)
+			text_file.write('\n\n')
+
+text_file.close();
